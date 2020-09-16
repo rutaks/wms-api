7f54f7d3d88fa034624e8253989a3f36c5b08fef
rutaks <rootsum.dev@gmail.com>
2020-09-16 06:34:27 +0200

    Initial Project Setup

---
+++ src/main/java/rw/rutaks/wms/api/WmsApiApplication.java
@@ -0,0 +1,13 @@
+package rw.rutaks.wms.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WmsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rw/rutaks/wms/api/WmsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package rw.rutaks.wms.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
