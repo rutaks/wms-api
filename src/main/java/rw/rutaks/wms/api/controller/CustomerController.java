678c258357f38915d766b3b487b651b7e16e0d51
rutaks <rootsum.dev@gmail.com>
2020-09-16 08:25:36 +0200

    Remove sendgrid from yml config

---
+++ src/main/java/rw/rutaks/wms/api/controller/CustomerController.java
@@ -0,0 +1,8 @@
+package rw.rutaks.wms.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {}
